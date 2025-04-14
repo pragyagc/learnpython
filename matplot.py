5115c0d6e858824d6d23d03be02b1b50a6524a56
pragya <pragyagc5@gmail.com>
2025-04-14 19:39:28 +0545

    Add matplotlib example script

--- matplot.py
+++ matplot.py
@@ -29,3 +29,32 @@ plt.title('Scatter Plot')
 
 # Display the plot
 plt.show()
+
+# create a bar plot of top ten climbers of Mt. Everest
+# Data
+categories = ['A', 'B', 'C', 'D']
+values = [10, 20, 15, 25]
+
+# Create a bar plot
+plt.bar(categories, values)
+
+# Add labels and title
+plt.xlabel('Categories')
+plt.ylabel('Values')
+plt.title('Bar Plot')
+
+# Display the plot
+plt.show()
+import pandas as pd
+df = pd.read_csv('../2. Fundamentals of NumPy and Pandas/mt_everest.csv')
+df.columns
+plt.bar(df['NAME'].value_counts().index[:10], df['NAME'].value_counts().values[:10])
+
+plt.xticks(rotation=90)
+# Add labels and title
+plt.xlabel('Climbers')
+plt.ylabel('No of times')
+plt.title('Top Ten Mt. Everest Climber')
+
+# Display the plot
+plt.show()
