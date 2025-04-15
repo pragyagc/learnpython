46bb4ce46ed826b933c2b32f2ac5aa4b59382176
pragya <pragyagc5@gmail.com>
2025-04-15 11:57:07 +0545

    array

---
+++ array.py
@@ -0,0 +1,8 @@
+import numpy as np
+a = np.array([1,2,3,4])
+b = np.array([5,6,7,8])
+print(a + b)
+arr_1d = np.array([1, 2, 3, 4, 5])
+arr_2d = np.array([[1, 2, 3,4], [4, 5, 6,5], [7, 8, 9,6]])
+print(arr_1d.ndim) # .size # .shape
+
